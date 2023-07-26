52a21e14a4a0f78923ba4b2295a47952d81c0a99
sarra2108 <ssass058@uottawa.ca>
2023-07-26 16:35:24 -0400

    my portfolio

---
+++ script.js
@@ -0,0 +1,54 @@
+
+    var tablinks=document.getElementsByClassName("tab-links");
+    var tabcontents=document.getElementsByClassName("tab-contents");
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+    var sidemenu = document.getElementById("sidemenu");
+    function openmenu(){
+        sidemenu.style.right ="0";
+
+    }
+    function closemenu(){
+        sidemenu.style.right ="-200px";
+        
+    }
+
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbxn617nFiRn4XjKk3QHNPB5sitSjvXUM1B4DXctouX-DmlhwaJvKYR8RAHKTFTyNbd0/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg= document.getElementById("msg")
+        form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
+        function showPopup() {
+  const popup = document.getElementById('popup');
+  
+  popup.style.display = 'block';
+}
+
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
+
+
+ 
+
+ 
